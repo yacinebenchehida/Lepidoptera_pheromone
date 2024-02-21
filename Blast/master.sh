08101cde8f6408c2b1b08209446e19321646d5e7
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-21 22:43:13 +0000

    Update master.sh

--- Blast/master.sh
+++ Blast/master.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-#SBATCH --mem=5GB
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --account=BIOL-SPECGEN-2018
-#SBATCH --time=0-2:00:00
-#SBATCH --job-name=blastx
-
-RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Results/$1"
-python3 ./blastx_multi_fasta.py $RESULTS/output_chunk_${SLURM_ARRAY_TASK_ID}.fasta ${SLURM_ARRAY_TASK_ID} $RESULTS/"$1"_db
-[ybc502@login1[viking2] Script]$ cat master.sh
-
 ################################
 # Load libraries and set paths #
 ################################
