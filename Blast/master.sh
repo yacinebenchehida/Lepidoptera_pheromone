0c615e563720d645f4b22d3dc14ea05070bfd594
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-25 21:25:57 +0000

    Update master.sh

--- Blast/master.sh
+++ Blast/master.sh
@@ -5,8 +5,8 @@ module load Biopython/1.81-foss-2022b
 module load BLAST+/2.14.0-gompi-2022b
 module load R/4.2.1-foss-2022a
 EDIRECT="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/edirect"
-RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Results/$1"
-PHEROMONE=$1
+RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Results/"
+PHEROMONE=$@
 
 mkdir -p $RESULTS
 
@@ -22,68 +22,72 @@ rm $RESULTS/Heliconius_melpomene_melpomene_Hmel2.5.cds.fa.gz
 ##############################
 # download data from uniprot #
 ##############################
-touch $RESULTS/db_"$PHEROMONE".fasta
+if [[ "$PHEROMONE" =~ "FAR" ]]; then
+    mkdir -p  $RESULTS/FAR
+    touch $RESULTS/FAR/db.fasta	
+    for i in uniprotkb uniparc uniref
+	do
+  		curl -L "https://rest.uniprot.org/${i}/stream?download=true&format=fasta&includeIsoform=true&query=%28%22fatty+acyl-coa+reductase%22+AND+%28taxonomy_id%3A7088%29%29" >> $RESULTS/FAR/db.fasta
+		echo "UNIPROT DATA FROM $i DOWNLOADED"
+	done
+fi
 
-if [ "$PHEROMONE" = "FAR" ]; then
+if [[ "$PHEROMONE" =~ "FAD" ]]; then
+    mkdir -p $RESULTS/FAD
+    touch $RESULTS/FAD/db.fasta
     for i in uniprotkb uniparc uniref
 	do
-  		curl -L "https://rest.uniprot.org/${i}/stream?download=true&format=fasta&includeIsoform=true&query=%28%22fatty+acyl-coa+reductase%22+AND+%28taxonomy_id%3A7088%29%29" >> $RESULTS/db_"$PHEROMONE".fasta
+		curl -L "https://rest.uniprot.org/${i}/stream?download=true&format=fasta&includeIsoform=true&query=%28%22fatty+acyl-CoA+desaturase%22+AND+%28taxonomy_id%3A7088%29%29" >> $RESULTS/FAD/db.fasta
 		echo "UNIPROT DATA FROM $i DOWNLOADED"
 	done
-elif [ "$PHEROMONE" = "FAD" ]; then
-    for i in uniprotkb uniparc uniref
-	do
-		curl -L "https://rest.uniprot.org/${i}/stream?download=true&format=fasta&includeIsoform=true&query=%28%22fatty+acyl-CoA+desaturase%22+AND+%28taxonomy_id%3A7088%29%29" >> $RESULTS/db_"$PHEROMONE".fasta
-		echo "UNIPROT DATA FROM $i DOWNLOADED"
-	done
-else
-    echo "unknown pheromone"
 fi
 
 ################################
 # download data from gene bank #
 ################################
-if [ "$PHEROMONE" = "FAR" ]; then
-		$EDIRECT/esearch -db Protein -query "fatty acyl-coa reductase[All Fields] AND Lepidoptera[Organism]"| $EDIRECT/efetch -format fasta >> $RESULTS/db_"$PHEROMONE".fasta
+if [[ "$PHEROMONE" =~ "FAR" ]]; then
+		$EDIRECT/esearch -db Protein -query "fatty acyl-coa reductase[All Fields] AND Lepidoptera[Organism]"| $EDIRECT/efetch -format fasta >> $RESULTS/FAR/db.fasta
 		echo GENBANK DATA DOWNLOADED
-elif [ "$PHEROMONE" = "FAD" ]; then
-		$EDIRECT/esearch -db Protein -query "fatty acyl-coa desaturase[All Fields] AND Lepidoptera[Organism]"| $EDIRECT/efetch -format fasta >> $RESULTS/db_"$PHEROMONE".fasta
+fi
+if [[ "$PHEROMONE" =~ "FAD" ]]; then
+		$EDIRECT/esearch -db Protein -query "fatty acyl-coa desaturase[All Fields] AND Lepidoptera[Organism]"| $EDIRECT/efetch -format fasta >> $RESULTS/FAD/db.fasta
 		echo GENBANK DATA DOWNLOADED
-else
-    echo "unknown pheromone"
 fi
 
 #####################################################################
 # Keep unique entries in the constitute database (removed doublons) #
 #####################################################################
-python3 unique_fasta.py $RESULTS/db_"$PHEROMONE".fasta "$PHEROMONE" > $RESULTS/unique_db_"$PHEROMONE".fasta
-mv *txt $RESULTS
-rm $RESULTS/db_"$PHEROMONE".fasta
-
-echo FINAL DATABASE CREATED
+for i in $PHEROMONE; do
+	python3 unique_fasta.py $RESULTS/$i/db.fasta "$i" > $RESULTS/$i/unique_db_"$i".fasta
+	mv *txt $RESULTS/$i
+	rm $RESULTS/$i/db.fasta
+	echo FINAL DATABASE CREATED
+done
 
 ################################################################
 # Create a blast data base with Heliconius melpomenes proteins #
 ################################################################
-makeblastdb -in $RESULTS/unique_db_"$PHEROMONE".fasta -dbtype prot -input_type fasta -out $RESULTS/"$PHEROMONE"_db -title "$PHEROMONE"_db
-echo BLAST DB CREATED
+for i in $PHEROMONE; do
+	makeblastdb -in $RESULTS/"$i"/unique_db_"$i".fasta -dbtype prot -input_type fasta -out $RESULTS/"$i"/"$i"_db -title "$i"_db
+	echo BLAST DB CREATED
+done
 
 ####################################################################
 # Create chunks of 200 fasta sequences (to parallelize the blastx) #
 ####################################################################
-python3 divide_fasta.py $RESULTS/Heliconius_melpomene_proteins.fa
-echo HELICONIUS PROTEINS FASTA SLICED
+for i in $PHEROMONE; do
+	cp $RESULTS/Heliconius_melpomene_proteins.fa $RESULTS/$i
+	python3 divide_fasta.py $RESULTS/$i/Heliconius_melpomene_proteins.fa
+	echo PROTEINS FASTA SLICED
+done
 
 #################################################
 # Run blastx on each sequence (and each chunks) #
 #################################################
-array_number=$(ls  $RESULTS/output_chunk_*|wc -l)
-
-for CHUNK in $RESULTS/chunk*
-do
-	sbatch --array=1-$array_number ./blastx.sh $PHEROMONE
-done
-
+for i in $PHEROMONE; do
+		array_number=$(ls  $RESULTS/$i/output_chunk_*|wc -l)
+		sbatch --job-name=Bl"$i" --array=1-$array_number ./blastx.sh $i
+	done
 echo BLASTx RAN
 
 #######################################################
@@ -94,15 +98,22 @@ python3 scaffold_size.py ../Inputs/Heliconius_melpomene_melpomene_Hmel2.5.scaffo
 #########################################
 # Combine results and extract best hits #
 #########################################
-running_jobs1=$(squeue|grep ybc502| grep blastx| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+running_jobs1=$(squeue|grep ybc502| grep BlFAR| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+running_jobs2=$(squeue|grep ybc502| grep BlFAD| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+
 echo $(eval echo "$running_jobs1")
 
-if [ "$PHEROMONE" = "FAR" ]; then
-                sbatch --job-name=COMB --dependency=aftercorr:$running_jobs1 ./combine_clean.sh $PHEROMONE 500
+if [[  "$PHEROMONE" =~ "FAR" ]]; then
+               sbatch --job-name=COFAR --dependency=aftercorr:$running_jobs1 ./combine_clean.sh FAD  500
                 echo DATA COMBINED
-elif [ "$PHEROMONE" = "FAD" ]; then
-                sbatch --job-name=COMB --dependency=aftercorr:$running_jobs1 ./combine_clean.sh $PHEROMONE 200
-                echo DATA COMBINED
-else
-    echo "unknown pheromone"
 fi
+if [[  "$PHEROMONE" =~ "FAD" ]]; then
+                sbatch --job-name=COFAD --dependency=aftercorr:$running_jobs2 ./combine_clean.sh FAR 200
+                echo DATA COMBINED
+fi
+
+########
+# Plot #
+########
+running_jobs3=$(squeue|grep ybc502| grep CO| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+sbatch --job-name=Plot  --dependency=aftercorr:$running_jobs3 ./plot.sh 
