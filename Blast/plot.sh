7d8a6935ce87ad85f9c1e1e8d6c1bf983fdbe637
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-26 00:43:44 +0000

    Create plot.sh

---
+++ Blast/plot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --mem=3GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=BIOL-SPECGEN-2018
+#SBATCH --time=0-00:10:00
+
+
+module load R/4.2.1-foss-2022a
+
+Rscript --vanilla Plot_chromosome.R
+mv FAD_FAR_Location.pdf ../Results
