28cd5859758d4b26b30aa44ff937bfbb8dba02e7
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-26 02:03:04 +0000

    Update combine_clean.sh

--- Blast/combine_clean.sh
+++ Blast/combine_clean.sh
@@ -14,6 +14,7 @@ cd  $RESULTS
 cat blast_hits_*|grep query|head -n 1 > combine_best_hits.txt
 cat blast_hits_*|grep -v query|awk -v biscore=$BITSCORE '$12 > biscore' >> combine_best_hits.txt
 cat blast_hits_*|grep -v query|awk -v biscore=$BITSCORE '$12 > biscore {print $1}'|sort -u > Protein_best_hits.txt
+cat Protein_best_hits.txt|while read line; do grep $line combine_best_hits.txt|head -n 1; done|awk '{print $1"\t"$12}' > BitScore.txt
 
 # Annotation file downloaded
 wget http://download.lepbase.org/v4/features/Heliconius_melpomene_melpomene_Hmel2.5.gff3.gz 
@@ -23,9 +24,10 @@ gunzip Heliconius_melpomene_melpomene_Hmel2.5.gff3.gz
 echo ANNOTATION FILE DECOMPRESSED
 
 #cat Protein_best_hits.txt |while read line; do grep $line Heliconius_melpomene_melpomene_Hmel2.5.gff3; done|grep mRNA|awk '{print $1"\t"$4"\t"$5}' >  location_Protein_best_hits.txt
-cat Protein_best_hits.txt | while read line; do paste <(echo "$line") <(grep "$line" Heliconius_melpomene_melpomene_Hmel2.5.gff3 | grep mRNA) | awk -v PHE="$PHE" '{print $1"\t"$2"\t"$5"\t"$6"\t"PHE"\t"$12 }'; done > location_Protein_best_hits.txt
+cat Protein_best_hits.txt | while read line; do paste <(echo "$line") <(grep "$line" Heliconius_melpomene_melpomene_Hmel2.5.gff3 | grep mRNA) <(grep $line BitScore.txt|awk '{print $2}')| awk -v PHE="$PHE" '{print $1"\t"$2"\t"$5"\t"$6"\t"PHE"\t"$11}'; done > location_Protein_best_hits.txt
 echo LOCATION OF $1 written
 #rm  output_chunk* blast_"$1"_hits_*
 rm output_chunk*
+
 
 python ../../Script/extract_fasta.py Protein_best_hits.txt Heliconius_melpomene_proteins.fa fasta_"$1"_protein.fa
