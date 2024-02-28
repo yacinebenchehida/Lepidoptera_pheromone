9b63c3db94d1acdba310650d8cb9bacda3f74269
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-28 17:36:16 +0000

    Update master.sh

--- Blast/master.sh
+++ Blast/master.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+#SBATCH --mem=5GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --account=BIOL-SPECGEN-2018
+#SBATCH --time=0-00:30:00
+
 ################################
 # Load libraries and set paths #
 ################################
@@ -5,19 +14,17 @@ module load Biopython/1.81-foss-2022b
 module load BLAST+/2.14.0-gompi-2022b
 module load R/4.2.1-foss-2022a
 EDIRECT="/mnt/scratch/projects/biol-specgen-2018/yacine/Tools/edirect"
-RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Results/"
-PHEROMONE=$@
+PHEROMONE=$(echo $@ | awk '{print $1"\t"$2}')
+SPECIES=$(echo $@ | awk '{print $3}')
+RESULTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Results/$SPECIES"
+INPUTS="/mnt/scratch/projects/biol-specgen-2018/yacine/Pheromones/Blast/Inputs/$SPECIES"
 
+################################
+# Download CDS in right format #
+################################
 mkdir -p $RESULTS
+cat $INPUTS/*cds.fa| perl -ne 's/[^\x00-\x7F]+/ /g; print;'|perl -pe 's/\.1//g' > $RESULTS/"$SPECIES"_proteins.fa
 
-###############################
-# Download CDS from melpomene #
-###############################
-wget http://download.lepbase.org/v4/sequence/Heliconius_melpomene_melpomene_Hmel2.5.cds.fa.gz
-mv Heliconius_melpomene_melpomene_Hmel2.5.cds.fa.gz $RESULTS
-echo MELPOMENE PROTEINS DOWNLOADED
-zcat $RESULTS/Heliconius_melpomene_melpomene_Hmel2.5.cds.fa.gz| perl -ne 's/[^\x00-\x7F]+/ /g; print;' > $RESULTS/Heliconius_melpomene_proteins.fa
-rm $RESULTS/Heliconius_melpomene_melpomene_Hmel2.5.cds.fa.gz
 
 ##############################
 # download data from uniprot #
@@ -59,14 +66,13 @@ fi
 #####################################################################
 for i in $PHEROMONE; do
 	python3 unique_fasta.py $RESULTS/$i/db.fasta "$i" > $RESULTS/$i/unique_db_"$i".fasta
-	mv *txt $RESULTS/$i
 	rm $RESULTS/$i/db.fasta
 	echo FINAL DATABASE CREATED
 done
 
-################################################################
-# Create a blast data base with Heliconius melpomenes proteins #
-################################################################
+##########################################
+# Create a blast data base with proteins #
+##########################################
 for i in $PHEROMONE; do
 	makeblastdb -in $RESULTS/"$i"/unique_db_"$i".fasta -dbtype prot -input_type fasta -out $RESULTS/"$i"/"$i"_db -title "$i"_db
 	echo BLAST DB CREATED
@@ -76,8 +82,8 @@ done
 # Create chunks of 200 fasta sequences (to parallelize the blastx) #
 ####################################################################
 for i in $PHEROMONE; do
-	cp $RESULTS/Heliconius_melpomene_proteins.fa $RESULTS/$i
-	python3 divide_fasta.py $RESULTS/$i/Heliconius_melpomene_proteins.fa
+	cp $RESULTS/"$SPECIES"_proteins.fa $RESULTS/$i
+	python3 divide_fasta.py $RESULTS/$i/"$SPECIES"_proteins.fa
 	echo PROTEINS FASTA SLICED
 done
 
@@ -86,34 +92,42 @@ done
 #################################################
 for i in $PHEROMONE; do
 		array_number=$(ls  $RESULTS/$i/output_chunk_*|wc -l)
-		sbatch --job-name=Bl"$i" --array=1-$array_number ./blastx.sh $i
+		SP=$(echo $SPECIES|cut -c 1-3)
+		NAME=$(echo "$SP""$i")
+		sbatch --job-name="$NAME" --array=1-$array_number ./blastx.sh $SPECIES $i
 	done
 echo BLASTx RAN
 
 #######################################################
 # Get size of each scaffold (used later for plotting) #
 #######################################################
-python3 scaffold_size.py ../Inputs/Heliconius_melpomene_melpomene_Hmel2.5.scaffolds.fa |awk '$3 > 1000000' > ../Inputs/scaffold_size_information.txt
+python3 scaffold_size.py ../Inputs/$SPECIES/*genome.fa |awk '$3 > 1000000' > ../Inputs/$SPECIES/scaffold_size_information.txt
+rm ../Inputs/$SPECIES/*genome.fa
 
 #########################################
 # Combine results and extract best hits #
 #########################################
-running_jobs1=$(squeue|grep ybc502| grep BlFAR| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
-running_jobs2=$(squeue|grep ybc502| grep BlFAD| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+SP=$(echo $SPECIES|cut -c 1-3)
+echo -e "${SP}FAR" 
+echo -e "${SP}FAD"
+
+running_jobs1=$(squeue|grep ybc502| grep -P "${SP}FAR"| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+running_jobs2=$(squeue|grep ybc502| grep -P "${SP}FAD"| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
 
 echo $(eval echo "$running_jobs1")
 
 if [[  "$PHEROMONE" =~ "FAR" ]]; then
-               sbatch --job-name=COFAR --dependency=aftercorr:$running_jobs1 ./combine_clean.sh FAR  350
+               sbatch --job-name="$SP"_R --dependency=aftercorr:$running_jobs1 ./combine_clean.sh $SPECIES FAR  350
                 echo DATA FAR COMBINED
 fi
 if [[  "$PHEROMONE" =~ "FAD" ]]; then
-                sbatch --job-name=COFAD --dependency=aftercorr:$running_jobs2 ./combine_clean.sh FAD 150
+                sbatch --job-name="$SP"_D --dependency=aftercorr:$running_jobs2 ./combine_clean.sh $SPECIES FAD 150
                 echo DATA FAD COMBINED
 fi
 
 ########
 # Plot #
 ########
-running_jobs3=$(squeue|grep ybc502| grep CO| awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
-sbatch --job-name=Plot  --dependency=aftercorr:$running_jobs3 ./plot.sh 
+running_jobs3=$(squeue|grep ybc502| grep -P "${SP}_" | awk '{print $1}'|perl -pe 's/\n/,/g'|sed 's/,$//g')
+echo $running_jobs3
+sbatch --job-name=Plot  --dependency=aftercorr:$running_jobs3 ./plot.sh $SPECIES
