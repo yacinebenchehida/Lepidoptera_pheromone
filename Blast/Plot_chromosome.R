438f8dac0c605a732df352a5503e31c41e462ca4
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-02-28 17:38:02 +0000

    Update Plot_chromosome.R

--- Blast/Plot_chromosome.R
+++ Blast/Plot_chromosome.R
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-#SBATCH --mem=3GB
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --account=BIOL-SPECGEN-2018
-#SBATCH --time=0-00:10:00
-
-
-module load R/4.2.1-foss-2022a
-
-Rscript --vanilla Plot_chromosome.R $1
-[ybc502@login2[viking2] Script]$ cat Plot_chromosome.R
 #################
 # Load packages #
 #################
