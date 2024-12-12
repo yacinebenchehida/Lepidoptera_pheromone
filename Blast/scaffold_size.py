787343bcd81f269d048d920ee7ae53bd498a1175
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2024-12-12 12:05:29 +0000

    Update scaffold_size.py

--- Blast/scaffold_size.py
+++ Blast/scaffold_size.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-
 from Bio import SeqIO
 import sys
 import os
 
+##################################################################
+# Command that look at the size of each sequence in a fasta file #
+##################################################################
 #usage: python -W ignore *fasta
 
-input_seq_iterator = SeqIO.parse(open(sys.argv[1], "rU"), "fasta")
+input_seq_iterator = SeqIO.parse(open(sys.argv[1], "r"), "fasta")
 
 for record in input_seq_iterator:
 	print(record.id + "\t" + str(1) + "\t"  + str(len(record.seq)))
